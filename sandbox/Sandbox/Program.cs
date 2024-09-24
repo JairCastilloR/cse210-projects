78862f03c765a0a98636f77c56c9da8734219f4f
jaircastillo <fjair.castillo@gmail.com>
2024-09-24 12:46:06 -0500

    Prep1 Inputs and Ouputs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello World!");
+        Console.Write("\n\nWhat is your first name?: ");
+        string first_name= Console.ReadLine();
+        Console.Write("\nWhat is your last name?: ");
+        string last_name= Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {last_name}, {first_name} {last_name}.\n");
+
     }
 }
