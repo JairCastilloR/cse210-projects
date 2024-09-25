78d874da4034720ef55d93d8f56036684e9284e4
jaircastillo <fjair.castillo@gmail.com>
2024-09-24 21:03:19 -0500

    Prep1 Inputs and Outputs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("\n\nWhat is your first name?: ");
+        string first_name= Console.ReadLine();
+        Console.Write("\nWhat is your last name?: ");
+        string last_name= Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {last_name}, {first_name} {last_name}.\n");
     }
 }
