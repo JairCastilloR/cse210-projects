e37de284373bba78efa2fd762d1883549d8d01af
jaircastillo <fjair.castillo@gmail.com>
2024-09-27 10:40:36 -0500

    prep3 Loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Hello Prep3 World!\n");
+        Random random_number = new Random();
+        int magic_number = random_number.Next(1, 100);
+
+        int guess_number = 0;
+
+        while (magic_number != guess_number)
+        {
+            Console.Write("What is your guess? ");
+            guess_number = int.Parse(Console.ReadLine());
+
+            if (magic_number > guess_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic_number < guess_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!\n");
+            }
+        }
     }
 }
